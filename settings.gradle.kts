59d7815094f8ea86ca639cf173620711859134fa
Zohaib Lalani <zohaiblalani@Zohaibs-MBP-2.lan>
2023-12-08 00:46:59 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application AUI 3220"
+include(":app")
